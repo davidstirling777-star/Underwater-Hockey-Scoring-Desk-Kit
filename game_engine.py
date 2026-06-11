6f5adb01871c4fedb0cb7cd01035343063e794a9
davidstirling777-star <davidstirling777@gmail.com>
2026-06-11 17:34:29 +1200

    Update game_engine.py

--- game_engine.py
+++ game_engine.py
@@ -1,7 +1,24 @@
 class GameEngine:
 
     def __init__(self):
-        print("GAME ENGINE INITIALIZED")
-
         self.white_goal_scorers = {}
         self.black_goal_scorers = {}
+
+    def record_goal_scorer(self, team, cap_number):
+
+        if cap_number is None:
+            return
+
+        if team == "White":
+            self.white_goal_scorers[cap_number] = (
+                self.white_goal_scorers.get(cap_number, 0) + 1
+            )
+
+        elif team == "Black":
+            self.black_goal_scorers[cap_number] = (
+                self.black_goal_scorers.get(cap_number, 0) + 1
+            )
+
+    def clear_goal_scorers(self):
+        self.white_goal_scorers.clear()
+        self.black_goal_scorers.clear()
