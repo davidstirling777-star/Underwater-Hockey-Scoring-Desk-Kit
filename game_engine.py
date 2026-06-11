415fdfa05a222328656c790500d0805a71576be9
davidstirling777-star <davidstirling777@gmail.com>
2026-06-11 12:48:52 +1200

    Update game_engine.py

--- game_engine.py
+++ game_engine.py
@@ -1,6 +1,7 @@
 class GameEngine:
 
     def __init__(self):
+        print("GAME ENGINE INITIALIZED")
 
-        self.white_score = 0
-        self.black_score = 0
+        self.white_goal_scorers = {}
+        self.black_goal_scorers = {}
