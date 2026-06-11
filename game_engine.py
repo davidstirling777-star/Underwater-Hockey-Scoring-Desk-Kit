bd3f1311deb88787f9017e27d254b8411bc26c56
davidstirling777-star <davidstirling777@gmail.com>
2026-06-11 18:08:27 +1200

    Update game_engine.py
    moving
    
    self.stored_penalties
    self.active_penalties

--- game_engine.py
+++ game_engine.py
@@ -4,6 +4,9 @@ class GameEngine:
         self.white_goal_scorers = {}
         self.black_goal_scorers = {}
 
+        self.stored_penalties = []
+        self.active_penalties = []
+
     def record_goal_scorer(self, team, cap_number):
 
         if cap_number is None:
@@ -22,3 +25,7 @@ class GameEngine:
     def clear_goal_scorers(self):
         self.white_goal_scorers.clear()
         self.black_goal_scorers.clear()
+
+    def clear_penalties(self):
+        self.stored_penalties.clear()
+        self.active_penalties.clear()
