80155fc9b5a355624b472d51b523e3846747676a
davidstirling777-star <davidstirling777@gmail.com>
2026-06-21 18:57:11 +1200

    Update zigbee_ui.py

--- zigbee_ui.py
+++ zigbee_ui.py
@@ -1,15 +1,9 @@
-import tkinter as tk
-from tkinter import ttk
-import webbrowser
-
-from zigbee_siren import is_mqtt_available
 
 import tkinter as tk
 from tkinter import ttk
 import webbrowser
 
 from zigbee_siren import is_mqtt_available
-
 
 def create_zigbee_siren_tab(app):
     """Create the Zigbee Siren configuration tab."""
