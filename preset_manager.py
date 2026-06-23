2e24decafe75d375151801a2f096b1a725655235
davidstirling777-star <davidstirling777@gmail.com>
2026-06-23 18:23:24 +1200

    Update preset_manager.py

--- preset_manager.py
+++ preset_manager.py
@@ -3,7 +3,6 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import time
-from settings_manager import save_preset_setting
 
 def open_button_dialog(app, idx, trigger_button=None):
     dialog_width = 400
